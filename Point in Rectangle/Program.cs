df5830b092acfe0f0ac04dfb481a760411eb08a2
Minh8x8 <105042689+Minh8x8@users.noreply.github.com>
2023-01-21 22:32:18 +0700

    hw 12/1/2023

---
+++ Point in Rectangle/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    public Point(){}
+}
+
+public class Rectangle
+{
+    public Point TopLeft { get; set; }
+    public Point BottomRight { get; set; }
+
+    public Rectangle(Point topLeft, Point bottomRight)
+    {
+        TopLeft = topLeft;
+        BottomRight = bottomRight;
+    }
+
+    public bool isInside(Point a)
+    {
+        return a.X >= BottomRight.X && a.X <= TopLeft.X && a.Y <= BottomRight.Y && a.Y >= TopLeft.Y;
+    }
+
+    public static void Main()
+    {
+        Point topLeft, bottomRight, point;
+        Console.WriteLine("Enter top left point");
+        string[] input = Console.ReadLine().Split(" ");
+        topLeft = new Point(int.Parse(input[0]), int.Parse(input[1]));
+        Console.WriteLine("Enter bottom right point");
+        string[] input2 = Console.ReadLine().Split(" ");
+        bottomRight = new Point(int.Parse(input2[0]), int.Parse(input2[1]));
+        Rectangle rect = new Rectangle(topLeft, bottomRight);
+        Console.WriteLine("Enter the point to check");
+        string[] input3 = Console.ReadLine().Split(" ");
+        point = new Point(int.Parse(input3[0]), int.Parse(input3[1]));
+        Console.WriteLine(rect.isInside(point));
+        
+    }
+}
---
+++ Student System/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.ComponentModel.Design;
+
+namespace StudentSystem
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Grade { get; set; }
+        public Student(){}
+
+        public Student(string name, int age, decimal grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}" +
+                              $"\nAge: {Age}" +
+                              $"\nGrade: {Grade}");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> stList = new List<Student>();
+            while (true)
+            {
+                MenuCommand();
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        CreateStudent(stList);
+                        break;
+                    case 2:
+                        ShowAStudentName(stList);
+                        break;   
+                    case 3:
+                        ShowAllStudent(stList);
+                        break;
+                }
+
+                if (choice == 4) break;
+            }
+        }
+
+        public static void MenuCommand()
+        {
+            Console.WriteLine("1. Create new student");
+            Console.WriteLine("2. Show a student's name");
+            Console.WriteLine("3. Show list of students");
+            Console.WriteLine("4. Exit");
+        }
+
+        public static void CreateStudent(List<Student> stList)
+        {
+            Student student = new Student();
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Grade: ");
+            decimal grade = decimal.Parse(Console.ReadLine());
+            student.Name = name;
+            student.Age = age;
+            student.Grade = grade;
+            stList.Add(student);
+        }
+
+        public static void ShowAStudentName(List<Student> stList)
+        {
+            Console.WriteLine("Enter the name of the student");
+            string name = Console.ReadLine();
+            foreach (Student student in stList)
+            {
+                if (student.Name == name)
+                {
+                    student.Show();
+                }
+            }
+        }
+
+        public static void ShowAllStudent(List<Student> stList)
+        {
+            foreach (Student student in stList)
+            {
+                student.Show();
+            }
+        }
+    }
+}

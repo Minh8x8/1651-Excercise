f1d1f1d65a3dadae7604170d9b4252b7d0177a1d
Minh8x8 <105042689+Minh8x8@users.noreply.github.com>
2023-02-04 13:39:07 +0700

    add hw 2-2-2023

---
+++ Hw 2-2-2023/Problem 4. Need forSpeed/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Problem_4._Need_forSpeed;
+
+public class Vehicle
+{
+    public int HorsePower { get; set; }
+    public double Fuel { get; set; }
+    public double DefaultFuelConsumption = 1.25;
+    public virtual double FuelConsumption()
+
+    public Vehicle(int horsePower, double fuel)
+    {
+        HorsePower = horsePower;
+        Fuel = fuel;
+    }
+    
+}
